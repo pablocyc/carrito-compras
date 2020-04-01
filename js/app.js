a22ace821ba69383d26a2ad394c31fb67d82a55d
Pablo Cardozo <pablocyc83@gmail.com>
2020-04-01 14:17:52 -0400

    Agregando en el carrito los cursos seleccionados

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Variables
 const carrito = document.getElementById('carrito')
 const cursos = document.getElementById('lista-cursos')
-
+const listaCursos = document.querySelector('#lista-carrito tbody')
 
 
 
@@ -25,12 +25,34 @@ function comprarCurso (e) {
   //Delegation para agregar-carrito
   if (e.target.classList.contains('agregar-carrito')) {
     const curso = e.target.parentElement.parentElement
-    // Enviamos el curso sellecionado para leer sus datos
+    // Enviamos el curso selecionado para leer sus datos
     leerDatosCurso(curso)
   }
 }
 
 // Lee los datos del curso
 function leerDatosCurso (curso) {
-  console.log(curso)
+  const infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id')
+  }
+
+  insertarCarrito(infoCurso)
+}
+
+function insertarCarrito (curso) {
+  const row = document.createElement('tr')
+  row.innerHTML = `
+    <td>
+      <img src="${curso.imagen}" />
+    </td>
+    <td>${curso.titulo}</td>
+    <td>${curso.precio}</td>
+    <td>
+      <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+    </td>
+  `
+  listaCursos.appendChild(row)
 }
